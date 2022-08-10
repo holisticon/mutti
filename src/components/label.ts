5bc3ba678aff5fb4671438d4611332ea33bac2c1
Christoph Fricke <christoph.fricke@holisticon.de>
2022-08-10 16:59:01 +0200

    feat(label): add host styles

--- src/components/label.ts
+++ src/components/label.ts
@@ -1,8 +1,20 @@
-import { html, LitElement, TemplateResult } from "lit";
+import { css, html, LitElement, TemplateResult } from "lit";
 import { customElement } from "lit/decorators.js";
+
+const styles = css`
+	:host {
+		box-sizing: border-box;
+		display: block;
+		width: 100%;
+		height: 100%;
+	}
+`;
 
 @customElement("mutti-label")
 export class MuttiLabelElement extends LitElement {
+	static override styles = styles;
+
+	readonly role = "rowheader";
 	override slot = "label";
 
 	protected override render(): TemplateResult {
