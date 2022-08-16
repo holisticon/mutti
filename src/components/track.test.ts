7dcb176c47e994dfcecee2fac0de62a967fa77e9
Christoph Fricke <christoph.fricke@holisticon.de>
2022-08-16 15:28:25 +0200

    feat(tracks): label tracks with containing labels
    refs: #3

--- src/components/track.test.ts
+++ src/components/track.test.ts
@@ -1,6 +1,8 @@
+import { screen } from "@testing-library/dom";
 import { html, render } from "../test-utils/render";
 import type { MuttiTrackElement } from "./track";
 import "./track";
+import "./label";
 
 describe("<mutti-track>", () => {
 	it("should set its slot property to track", async () => {
@@ -17,5 +19,27 @@ describe("<mutti-track>", () => {
 		);
 		expect(element).toHaveSlot("label");
 		expect(element).toHaveSlot("item");
+	});
+
+	it("should label itself with a containing <mutti-label>", async () => {
+		const { element } = await render<MuttiTrackElement>(
+			html`<mutti-track><mutti-label>Test</mutti-label></mutti-track>`
+		);
+
+		const label = screen.getByText("Test");
+
+		expect(label.id).toBe("mutti-label-0");
+		expect(element.getAttribute("aria-labelledby")).toBe(label.id);
+	});
+
+	it("should preserve the id of its label if exists", async () => {
+		const { element } = await render<MuttiTrackElement>(
+			html`<mutti-track><mutti-label id="test">Test</mutti-label></mutti-track>`
+		);
+
+		const label = screen.getByText("Test");
+
+		expect(label.id).toBe("test");
+		expect(element.getAttribute("aria-labelledby")).toBe(label.id);
 	});
 });
